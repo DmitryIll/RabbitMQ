eb9b90e4506f340f71657669933c2137a5dbde3a
DmitryIll <dnillarionov@gmail.com>
2024-02-06 21:19:42 +0300

    4

--- scripts/producer.py
+++ scripts/producer.py
@@ -3,6 +3,7 @@
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+#connection = pika.BlockingConnection(pika.URLParameters('amqp://test:passwd@rabbitmq2:5672')) 
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
